1d0eecd115133167722945d4f6f1ecaf9a94ff02
QiHong Lu <lvqihong1992@gmail.com>
2018-04-02 12:34:26 -0400

    switch to int representation of the world

--- episodic_memory/utils.py
+++ episodic_memory/utils.py
@@ -2,6 +2,7 @@ from functools import reduce
 import operator
 import numpy as np
 from string import ascii_lowercase
+from itertools import product
 
 
 def extract_param(episode):
@@ -64,13 +65,24 @@ def flatten_lists(lists):
     return lists
 
 
+# def attach_suffix(item_list, suffix):
+#     """ add a int suffix to a parameter vector
+#         letter representation of the parameter
+#     """
+#     vals = []
+#     # generate instantiation of the underlying parameter
+#     for item in item_list:
+#         vals.append(item.lower() + '%d' % (suffix))
+#     return vals
+
 def attach_suffix(item_list, suffix):
     """ add a int suffix to a parameter vector
+        int representation of parameters
     """
     vals = []
     # generate instantiation of the underlying parameter
     for item in item_list:
-        vals.append(item.lower() + '%d' % (suffix))
+        vals.append([int(item), int(suffix)])
     return vals
 
 
@@ -121,6 +133,34 @@ def vals_to_one_hot_vecs(values, k, value_set=list(ascii_lowercase)):
         sub_one_hot_vec = val_to_one_hot_vec(val, k)
         full_one_hot_vec[start_idx:end_idx] = sub_one_hot_vec
     return full_one_hot_vec
+
+
+def remove_repeated_list(ll):
+    set_of_list = list()
+    for sublist in ll:
+        if sublist not in set_of_list:
+            set_of_list.append(sublist)
+    return set_of_list
+
+
+def intersection_ll(ll1, ll2):
+    intersection_list = []
+    for list_l1 in ll1:
+        if list_l1 in ll2:
+            intersection_list.append(list_l1)
+    return intersection_list
+
+
+def compute_ll_overlap(lll):
+    """
+    input: list of lists of lists
+    """
+    n_ll = len(lll)
+    overlap_map = np.zeros((n_ll, n_ll))
+    for i, j in product(range(n_ll), range(n_ll)):
+        sit_intersection = intersection_ll(lll[i], lll[j])
+        overlap_map[i, j] = len(sit_intersection)
+    return overlap_map
 
 
 """ simple helper functions
--- episodic_memory/world.py
+++ episodic_memory/world.py
@@ -1,11 +1,12 @@
 import numpy as np
 from episodic_memory.utils import flatten_lists, attach_suffix, \
-    get_set_intersection
+    remove_repeated_list, intersection_ll, compute_ll_overlap
 from string import ascii_uppercase
+import sys
 
 
 class World():
-    def __init__(self, param_vec_dim, num_ongoing_vecs, max_realization_idx,
+    def __init__(self, param_vec_dim, num_ongoing_vecs, num_branches,
                  param_vecs=None):
         # TODO if param_vecs is provided, should check consistency
         # the dimension of the parameter vector
@@ -13,7 +14,7 @@ class World():
         # the number of ongoing parameter vectors
         self.num_ongoing_vecs = num_ongoing_vecs
         # the number of possible realizations of any parameter (0=UNK)
-        self.max_realization_idx = max_realization_idx
+        self.num_branches = num_branches
         # if no pre-defined param_vecs, take random samples
         if param_vecs == None:
             self.__initialize_param_vectors()
@@ -24,7 +25,7 @@ class World():
 
     def __initialize_param_vectors(self):
         max_shifts = 1
-        _, param_vecs_with_shift_s = gen_param_vecs(self.param_vec_dim, False)
+        _, param_vecs_with_shift_s = gen_param_vecs(self.param_vec_dim)
         vec_set = select_param_vecs_with_shift_s(
             param_vecs_with_shift_s, max_shifts)
         self.param_vecs = vec_set
@@ -34,6 +35,7 @@ class World():
         """
         if num_events < 0:
             raise ValueError('num_events and event_length are natural numbers')
+        self.clear()
         self.num_events = num_events
         for i in range(num_events):
             situation = self.__sample_latent_causes(verbose)
@@ -84,20 +86,20 @@ class World():
         pred_seq = sample_value_source(val_source)
         return event_seq, pred_seq
 
-    def __sample_latent_causes(self, verbose=False):
+    def __sample_latent_causes(self, verbose=True):
         """ randomly sample some parameter vectors
             instantiate the vectors
         """
         # sample some vectors as the source
         num_param_vecs = len(self.param_vecs)
-        params_indices = np.random.choice(
+        selected_param_ids = np.random.choice(
             num_param_vecs, size=self.num_ongoing_vecs, replace=False, p=None)
-        vec_source = [self.param_vecs[i] for i in params_indices]
+        vec_source = [self.param_vecs[i] for i in selected_param_ids]
         # instantiate the parameter vector sets
         val_source_lists = self.__instantiate_vec_source(vec_source)
-        # construct a situation
+        # construct a situation by combining all value vectors
         situation = flatten_lists(val_source_lists)
-        situation = sorted(list(set(situation)))
+        situation = remove_repeated_list(situation)
 
         # take a snapshot for display purpose
         self.vec_sources.append(vec_source)
@@ -137,45 +139,38 @@ class World():
         return val_source
 
     def __get_random_suffix(self):
-        """ get a random index for the episodes, avoid zero
+        """Get a random index for the episodes (= the choice of branch)
         """
+        # plus 1 to avoid zero
         suffix = np.random.choice(
-            np.arange(self.max_realization_idx), size=1, replace=True) + 1
+            np.arange(self.num_branches), size=1, replace=True) + 1
         return suffix
 
-    def print_info(self):
-
-        def compute_source_overlap():
-            n_sources = len(self.situations)
-            overlap_heatmap = np.zeros((n_sources, n_sources))
-            for i in range(n_sources):
-                for j in range(n_sources):
-                    overlap_heatmap[i, j] = len(get_set_intersection(
-                        self.situations[i], self.situations[j]))
-            return overlap_heatmap
-
-        print('--- WORLD INFO ---')
-        print('Number of ongoing vectors: %s' % self.num_ongoing_vecs)
-        print('Parameter vector dimension: %s' % self.param_vec_dim)
-        print('All param vectors: ', self.param_vecs)
+    def __repr__(self):
+        info = '--- WORLD INFO ---\n'
+        info += 'Number of ongoing vectors: %s\n' % self.num_ongoing_vecs
+        info += 'Parameter vector dimension: %s\n' % self.param_vec_dim
+        info += 'All param vectors: %s\n' % self.param_vecs
         if self.num_events == 0:
-            return
-        print('Event parameters:')
+            return info
         for i in range(self.num_events):
-            print('- Situation %d:' % i)
-            print('-- Vecs:', self.vec_sources[i])
-            print('-- Vals:', self.val_sources[i])
+            info += '- Situation: %d\n' % (i)
+            info += '-- Vecs: %s\n' % (self.vec_sources[i])
+            info += '-- Vals: %s\n' % (self.val_sources[i])
+        overlap_heatmap = compute_ll_overlap(self.situations)
+        info += '-- situation overlapmap: \n%s' % (overlap_heatmap)
+        return info
 
-        overlap_heatmap = compute_source_overlap()
-        print('-- situation overlapmap: \n', overlap_heatmap)
-        print('\n')
+
+""" helper function """
+
+DEFAULT_PARAMS_SET = np.arange(1, 27, 1)
 
 
-def gen_param_vecs(param_dim, verbose=False):
+def gen_param_vecs(param_dim, params_set=DEFAULT_PARAMS_SET, verbose=False):
     """ generate parameter vectors (consecutive letters)
     """
     max_overlap_num = param_dim - 1
-    params_set = ascii_uppercase
     num_params = len(params_set)
     # preallocate
     all_param_vecs = []
@@ -202,71 +197,49 @@ def select_param_vecs_with_shift_s(param_vecs_with_shift_s, max_shifts):
     return vec_set
 
 
-# def instantiate_param_vecs(vec_set, max_realization_idx):
-#     """ instantiate a set of parameter vectors with shifts
-#     """
-#     # instantiate all vectors with all possible realization indices
-#     instantiated_vec_set = []
-#     for param_vector in vec_set:
-#         instantiated_vec = [attach_suffix(param_vector, idx + 1)
-#                             for idx in range(max_realization_idx)]
-#         instantiated_vec_set.append(instantiated_vec)
-#     return instantiated_vec_set
+# """ demos """
 
-
-""" demos """
-
-
-def get_naive_param_vec_set():
-    mid_pt = 13
-    param_vecs = [[ascii_uppercase[i] for i in range(mid_pt)],
-                  [ascii_uppercase[i+mid_pt]
-                   for i in range(len(ascii_uppercase) - mid_pt)]
-                  ]
-    return param_vecs
-
-
-""" example 1: how to use
-"""
+# """ example 1: how to use
+# """
 # # world constants
 # param_vec_dim = 4
 # num_ongoing_vecs = 4
-# max_realization_idx = 2
+# num_branches = 2
 # # event constants
 # num_events = 2
 # event_length = 10
 #
-# w0 = World(param_vec_dim, num_ongoing_vecs, max_realization_idx)
-# w0.print_info()
+# w0 = World(param_vec_dim, num_ongoing_vecs, num_branches)
+# w0
+#
 # w0.initialize_situations(num_events, event_length)
-# w0.print_info()
+# w0
+# w0.make_event_seq(event_length)
 
-
-""" example 2: naive World
-    non-overlapping event structure
-    number of ongoing vector = 1
-"""
-# naive_param_vecs = get_naive_param_vec_set()
+#
+# """ example 2: naive World
+#     non-overlapping event structure
+#     number of ongoing vector = 1
+# """
+# naive_param_vecs = [[(i+1) for i in range(10)], [(i+11) for i in range(10)]]
 # # constants
 # param_vec_dim = len(naive_param_vecs[0])
-# max_realization_idx = 1
+# num_branches = 2
 # num_ongoing_vecs = 1
 # num_events = 1
 # event_length = 30
 #
 # # set up world 0
-# w0 = World(param_vec_dim, num_ongoing_vecs, max_realization_idx,
+# w0 = World(param_vec_dim, num_ongoing_vecs, num_branches,
 #            [naive_param_vecs[0]])
-# # w0.print_info()
 # w0.initialize_situations(num_events)
-# w0.print_info()
+# w0
 #
 # # set up world 1
-# w1 = World(param_vec_dim, num_ongoing_vecs, max_realization_idx,
+# w1 = World(param_vec_dim, num_ongoing_vecs, num_branches,
 #            [naive_param_vecs[1]])
-# # w1.print_info()
 # w1.initialize_situations(num_events)
-# w1.print_info()
+# w1
 #
 # # make events
 # obs_seq0, pred_seq0 = w0.make_event_seq(event_length)
